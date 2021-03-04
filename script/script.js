35d370a85eaa3868bc91ff2e3eed20b737ce001c
Kevin <kevisrich144@gmail.com>
2021-03-04 15:34:12 +0000

    Slider

--- script/script.js
+++ script/script.js
@@ -7,4 +7,12 @@ $(document).ready(function(){
     $(".item").on('click', function(){
         $("#open-menu").slideUp();
     })
+
+    $("#scroll").on('click', function(event){
+        event.preventDefault();
+        //console.log("Hello");
+        $("html,#body").animate({scrollTop: 0}, 500);
+        //$("#scroll").animate({"top":"-=300"}, 500);
+        // Moves the scrool arrow up until its out of the page.
+    });
 })
